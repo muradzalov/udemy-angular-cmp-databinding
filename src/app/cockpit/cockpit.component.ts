9166698c6232d427dc46507b375794e63bec17c3
muradzalov <mzalov@gmail.com>
2021-07-18 10:20:10 -0500

    Updating based on the latest changes

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 // import { EventEmitter } from 'stream';
 
 @Component({
@@ -8,21 +8,29 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
   newServerContent = '';
+  @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // Only reason this now has a parameter of nameInput is that we're using a local reference in the cockpit.component.html file and demonstrating that there is a way to pass data without two-way data binding
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(this.serverContentInput)
+    // console.log(nameInput.value);
+    this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
   }
 
+  // onAddBlueprint() {
+  //   this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  // }
   onAddBlueprint() {
-    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.serverContentInput.nativeElement.value});
   }
+
 
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,10 +7,57 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
-  constructor() { }
+  constructor() {
+    // Called first
+    console.log('Constructor called!');
+   }
+
+   ngOnChanges(changes: SimpleChanges) {
+     // called after a bound input property changes
+    console.log('ngOnChanges called!', changes);
+  }
 
   ngOnInit(): void {
+    // called once the component is initialized
+    console.log('ngOnInit called!')
+    console.log('Text Content: ', this.header.nativeElement.textContent);
+    console.log('Text Content of paragraph: ', this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck() {
+    // called during every change detection run
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    // called after content (ng-content) has been projected into view
+    console.log('ngAfterContentInit called!');
+    console.log('Text Content of paragraph: ', this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked() {
+    // called every time the projected content has been checked
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    // called after the component’s view (and child views) has been initialized
+    console.log('ngAfterViewInit called!');
+    console.log('Text Content: ', this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    // called every time the view (and child views) have been checked
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() { 
+    // called right before the component is about to be destroyed
+    console.log('ngOnDestroy called!');
   }
 
 }
